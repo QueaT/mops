7f7eeec3314cbc381692142d7886da394c30e996
Jakub <jgawronski@9bits>
2019-06-16 00:51:21 +0200

    test:

--- working.js
+++ working.js
@@ -1,28 +1,37 @@
+import pos from '../positionOfElementengine.js';
+
 const photoDog = document.querySelector(".image--dog");
 const button = document.querySelector('.startButton');
 const fonts = document.querySelectorAll('.additional');
 const start = document.querySelector(".startButton");
 const finish = document.querySelector(".finish");
+const l = new pos();
+l.initialValue = [200,200];
 
 function disappearButton(){
 start.classList.toggle("active");
 finish.classList.add("active");
+function disappearButton() {
+    start.classList.toggle("active");
+    finish.classList.add("active");
+
 }
 start.addEventListener("click", disappearButton);
 
-
-button.addEventListener('click',()=>{
-fonts.forEach(font =>{
-   font.classList.add('active');
-  })
+button.addEventListener('click', () => {
+    fonts.forEach(font => {
+        font.classList.add('active');
+    })
 })
+
+
 function photoMove(e) {
     const radius = 350 / 2;
     const posX = e.pageX - radius;
     const documentWidth = window.innerWidth;
     if (e.pageX >= 350 / 2 && e.pageX < documentWidth - radius) {
         photoDog.style.left = posX + 'px';
-    } 
+    }
 }
 
 photoDog.addEventListener("mousemove", photoMove);
