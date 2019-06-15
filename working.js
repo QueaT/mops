5eea69ffb4dbae1a3545643ddf9687241ee3eaff
Jakub <jgawronski@9bits>
2019-06-15 22:38:47 +0200

    test:

--- working.js
+++ working.js
@@ -3,24 +3,27 @@ const button = document.querySelector('button');
 const fonts = document.querySelectorAll('.fas');
 const start = document.querySelector(".startButton");
 
-function disappearButton(){
-start.classList.toggle("active");
+function disappearButton() {
+    start.classList.toggle("active");
 }
 start.addEventListener("click", disappearButton);
 
 
-button.addEventListener('click',()=>{
-fonts.forEach(font =>{
-   font.classList.add('active');
-  })
+button.addEventListener('click', () => {
+    fonts.forEach(font => {
+        font.classList.add('active');
+        const elementY = font.getClientRects()[0].top
+        console.log(elementY);
+    })
 })
+
 function photoMove(e) {
     const radius = 350 / 2;
     const posX = e.pageX - radius;
     const documentWidth = window.innerWidth;
     if (e.pageX >= 350 / 2 && e.pageX < documentWidth - radius) {
         photoDog.style.left = posX + 'px';
-    } 
+    }
 }
 
 photoDog.addEventListener("mousemove", photoMove);
